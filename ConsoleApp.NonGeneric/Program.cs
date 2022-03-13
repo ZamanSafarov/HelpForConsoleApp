50a018f2115a52908ac2f16f35d97ff695d16ba1
Zaman <zaman.safarov123456@gmail.com>
2022-03-13 19:10:18 +0400

    s

---
+++ ConsoleApp.NonGeneric/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp.NonGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("demo");
+            list.Add("demo1");
+            list.Add("dem1");
+            list.Add("dem112");
+           // list.Clear();
+            Console.WriteLine("-----------------");
+            Console.WriteLine(list[1]);
+            Console.WriteLine("--------salm-----------");
+            
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
